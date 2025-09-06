d864272fa3a2dd72324a164e5aaeeaffc34a01c8
01kevinms <kevin.victors29@gmail.com>
2025-09-05 21:19:05 -0300

    devmovie

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,22 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 
-import Detail  from "../containers/Detail";
+import Detail from "../containers/Detail";
 import Home from '../containers/home'
 import Movies from '../containers/Movies'
 import Series from '../containers/Series'
 import Defaultlayout from '../layout/Defaultlayout'
 
 function Router() {
-
-    return (
-        <Routes>
-            <Route element={<Defaultlayout />}>
-                <Route path='/' element={<Home />} />
-                <Route path='/filmes' element={<Movies />} />
-                <Route path='/series' element={<Series />} />
-                <Route path='/detalhe/:id' element={<Detail />} />
-            </Route>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route element={<Defaultlayout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/filmes' element={<Movies />} />
+        <Route path='/series' element={<Series />} />
+        <Route path='/detalhe/:id' element={<Detail />} />
+        <Route path='*' element={<h1>Página não encontrada</h1>} /> {/* 👈 fallback */}
+      </Route>
+    </Routes>
+  )
 }
 export default Router
